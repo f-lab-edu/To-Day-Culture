3633f5cc2c697c6f3395ff8729b2db77bc9f5ccb
wktls63 <wktls63@naver.com>
2024-10-14 00:06:09 +0900

    chore(user-service): Change directory for conftest.py

--- services/user-service/tests/conftest.py
+++ services/user-service/tests/conftest.py
@@ -1,5 +1,6 @@
-import sys
 import os
+import sys
 
-# 프로젝트의 루트 경로를 PYTHONPATH에 추가
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
+# 프로젝트 루트 경로 설정
+root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(root_path)
